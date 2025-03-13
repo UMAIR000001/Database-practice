53d78741ff940d81fb878fe0db58ae33efd6babc
Mohammad Umair Hafeez <umairhafeez951@gmail.com>
2025-03-13 23:24:03 +0500

    Basic database pracitce in posrtges SQL

---
+++ database_practice.sql
@@ -0,0 +1,4 @@
+--create table demo_table(id int primary key ,name varchar(50),majors varchar(50))
+
+--insert into demo_table values (1,'Umair','Computer Science'),(2,'Baloch','CSS'),(3,'Buzdar','Data Science'),(4,'Sam','AI'),(5,'Khan','IT'),(6,'alex','Data Analyst');
+select id from demo_table where id>3 and id<=6
